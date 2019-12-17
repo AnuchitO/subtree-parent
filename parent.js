c020168aa86fa631bee019ddbe6d1b422f3b3fd1
AnuchitO <anuchitp@thoughtworks.com>
2019-12-17 12:58:51 +0700

    update parent.js

--- parent.js
+++ parent.js
@@ -1,5 +1,3 @@
 const child = require('./subtree-child/child')
 console.log("this is parent subtree.")
-
-child.doSomething("new param", "cow param")
-child.doSomething("new param", "cow param")
+child.doSomething('Tobias')
