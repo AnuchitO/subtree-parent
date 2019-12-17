5bed750a82b33db5e6c7305fe6569cde0ae6a36f
AnuchitO <anuchitp@thoughtworks.com>
2019-12-17 12:56:52 +0700

    [subtree] update friend

--- subtree-child/child.js
+++ subtree-child/child.js
@@ -1,7 +1,6 @@
-const doSomething = (newparam, moreparam, ownChange) => {
-  console.log(`new param ${newparam}::::${moreparam}:::${ownChange}`)
+const doSomething = (friend) => {
   console.log('this is a child subtree')
-  console.log('udpate subtree 1')
+  console.log(`friend ${friend}`)
 }
 
 exports.doSomething = doSomething
